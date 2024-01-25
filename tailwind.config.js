9d2273d5d7feb8a0812fe2b418aa28aaefe107e6
PramAhmad <pramuditaadit2810@gmail.com>
2024-01-26 00:52:15 +0700

    update typhograpy:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,5 @@
-// this config file is used to overide the default tailwindcss settings for my life to better a easliasy styling with framework 
-// for no reason i use tailwind css
+module.exports = {
+    daisyui: {
+      darkTheme: 'cmyk',
+    },
+  };
